24488fcf17b804f7149d4893bf99adaf30863072
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-10-05 16:52:08 -0700

    Product Data

--- products/data.js
+++ products/data.js
@@ -1,10 +1,43 @@
 export const spirits = [
+
     {
-        id: 'happyChild',
+        id: 'flowerChild',
         name: 'Flower Child',
         image: 'lavendarSpirit.jpg',
-        description: 'Lovely young spirit from the 1970s. Will occassionally play records. A gardener in life, will help your flowers bloom.',
+        description: 'Lovely young spirit from the 1970s. Loves music. A gardener in life, will help your flowers bloom.',
         category: 'Happy',
         price: 799,
+    },
+    {
+        id: 'princessChild',
+        name: 'Pretty Princess',
+        image: 'pinkSpirit.jpg',
+        description: 'Lively spirit from the 1990s. Always leaves glitter behind. Singing is guaranteed.',
+        category: 'Happy',
+        price: 599,
+    },
+    {
+        id: 'bookishSpirit',
+        name: 'Dedicated Reader',
+        image: 'purpleSpirit.jpg',
+        description: 'Late 1800s spirit. Fond of reading. Recommended for homes with expansive libraries.',
+        category: 'Happy',
+        price: 799,
+    },
+    {
+        id: 'puppySpirit',
+        name: 'Puppy Power',
+        image: 'purpleSpirit2.jpg',
+        description: 'Canine spirit, date unknown. Loves fetch, though cannot bring back stick.',
+        category: 'Happy',
+        price: 399,
+    },
+    {
+        id: 'grandmaCookies',
+        name: 'Doting Grandmother',
+        image: 'yellowSpirit.jpg',
+        description: 'Older spirit, circa 1950s. Will assist with recipes in kitchen. Always smells like fresh-baked cookies.',
+        category: 'Happy',
+        price: 899,
     }
 ];

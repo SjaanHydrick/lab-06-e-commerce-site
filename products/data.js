16bd903751ddc765082e9de7e1115917b1510c90
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-10-05 15:36:40 -0700

    Design List

---
+++ products/data.js
@@ -0,0 +1,10 @@
+export const spirits = [
+    {
+        id: 'happyChild',
+        name: 'Flower Child',
+        image: 'lavendarSpirit.jpg',
+        description: 'Lovely young spirit from the 1970s. Will occassionally play records. A gardener in life, will help your flowers bloom.',
+        category: 'Happy',
+        price: 799,
+    }
+];
---
+++ products/utils.js
@@ -0,0 +1,37 @@
+// id: 'happyChild',
+// name: 'Flower Child',
+// image: 'lavendarSpirit.jpg',
+// description: 'Lovely young spirit from the 1970s. Will occassionally play records. A gardener in life, will help your flowers bloom.',
+// category: 'Happy',
+// price: 799,
+
+export function renderSpirit(spirit) {
+    const li = document.createElement('li');
+    const name = document.createElement('p');
+    const image = document.createElement('img');
+    const price = document.createElement('p');
+    const button = document.createElement('button');
+
+    li.classList.add('spirit');
+
+    name.classList.add('name');
+    name.textContent = spirit.name;
+
+    li.appendChild(name);
+
+    image.src = `../Spirits/HappySpirits/${spirit.image}`;
+
+    li.appendChild(image);
+
+    price.classList.add('price');
+    price.textContent = `$${spirit.price.toFixed(2)}`;
+
+    li.appendChild(price);
+
+    button.textContent = 'Add to Cart';
+
+    li.appendChild(button);
+
+    return li;
+
+}

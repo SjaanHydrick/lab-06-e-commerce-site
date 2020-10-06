c59f8d93d77e854d7e71a4fcdd6479babf3f0ccc
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-10-06 14:43:47 -0700

    Cart design and Cart data complete

---
+++ shopping-cart/cart.js
@@ -0,0 +1,16 @@
+export const cart = [
+
+    {
+        id: 'flowerChild',
+        quantity: 1,
+    },
+    {
+        id: 'bookishSpirit',
+        quantity: 1,
+    },
+    {
+        id: 'fishGhost',
+        quantity: 1,
+    }
+
+];

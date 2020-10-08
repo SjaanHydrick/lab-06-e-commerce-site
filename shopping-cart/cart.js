06d7e847a158342ac9385fe8a37cc269140d5ef7
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-10-07 17:48:56 -0700

    fixing take to main page

--- shopping-cart/cart.js
+++ shopping-cart/cart.js
@@ -30,7 +30,7 @@ orderButton.addEventListener('click', () => {
         alert(stringyCart);
 
         localStorage.clear();
-        window.location.href = '/';
+        window.location.href = '/lab-06-e-commerce-site/index.html';
     } else {
         orderButton.disabled = true;
     }
